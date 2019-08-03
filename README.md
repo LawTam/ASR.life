9cd6a68f841c6626f285360ae3ca32a374bcbb0c
Lawrence Tam <lawhtam@gmail.com>
2019-08-03 11:36:22 -0700

    readme updated

--- README.md
+++ README.md
@@ -3,11 +3,17 @@ Website for Adrenaline Sports Resort
 
 Will be posted to http://asr.life/
 
+Work cycle: 
+
+PLANNING BEGINS> 05/21/2019
+
+DEVELOPMENT> 07/29/2019 - current
+
 # Technologies Used
-HTML, CSS, Javascript
+HTML, CSS, Javascript, Jquery
 
 Node.js & Express.js -- useful website development architecture and API || https://expressjs.com/en/starter/generator.html
 
-Jade template engine -- useful for clean webpage building || https://naltatis.github.io/jade-syntax-docs/
+Jade/Pug template engine -- useful for clean webpage building || https://naltatis.github.io/jade-syntax-docs/
 
 Adobe XD -- free wireframing tool built by Adobe || https://www.adobe.com/products/xd.html
--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var app = express();
+
+// Set favicon (website/bookmark icon)
+app.use(favicon(path.join(__dirname, 'icon.ico')))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,7 +34,7 @@ app.use('/', Homepage_Router);
 app.use('/About_Us', About_Us_Router);
 
 
-// // catch 404 and forward to error handler
+// catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
 //   res.status(404).send("Sorry can't find that!")
